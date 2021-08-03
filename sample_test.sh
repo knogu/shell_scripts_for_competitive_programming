ce7f18e93039999fccb92e539093ac2ead2fdec2
knogu <kotaronoguchi61@gmail.com>
2021-08-03 16:09:08 +0900

    icecreamやcppのdumpをコメントアウトするように変更

--- sample_test.sh
+++ sample_test.sh
@@ -1,3 +1,6 @@
 filename=${1:-main.py}
 sed -i -e "s/\(^[ ]*\)mt/\1# mt/g" $filename $filename
+sed -i -e "s/\(^[ ]*\)ic/\1# ic/g" $filename $filename
+sed -i -e "s/^\(from icecream import ic\)/# \1/g" $filename $filename
+sed -i -e "s/\(^[ ]*\)dump/\1\/\/ dump/g" $filename $filename
 atcoder-tools test
