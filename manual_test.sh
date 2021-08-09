bf7e872fd5c56aac6308a00df56fe4de560f49cb
knogu <kotaronoguchi61@gmail.com>
2021-08-09 15:23:46 +0900

    icのインポートを、手動テスト時にコメントインできるようにした

--- manual_test.sh
+++ manual_test.sh
@@ -1,3 +1,4 @@
 filename=${1:-main.py}
 sed -i -e "s/\(^[ ]*\)mt(\([^'\"]*\))/\1mt('\2', \2)/g" $filename $filename
+sed -i -e "s/#[ #]*from icecream import ic/from icecream import ic/g" $filename $filename
 python $filename
