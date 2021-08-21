6bfd3de7fc072b6a7c396edfa4d817f6fef810bc
knogu <kotaronoguchi61@gmail.com>
2021-08-21 19:12:20 +0900

    ファイル名の変数の命名をfileに統一

--- add_argname.sh
+++ add_argname.sh
@@ -1,2 +1,2 @@
-filename=${1:-main.py}
-sed -i -e "s/\(^[ ]*\)db(\([^'\"]*\))/\1db('\2', \2)/g" $filename $filename
+file=${1:-main.py}
+sed -i -e "s/\(^[ ]*\)db(\([^'\"]*\))/\1db('\2', \2)/g" $file $file
--- comment_out.sh
+++ comment_out.sh
@@ -1,5 +1,5 @@
-filename=${1:-main.py}
-sed -i -e "s/\(^[ ]*\)db/\1# db/g" $filename $filename
-sed -i -e "s/\(^[ ]*\)ic/\1# ic/g" $filename $filename
-sed -i -e "s/^\(from icecream import ic\)/# \1/g" $filename $filename
-sed -i -e "s/\(^[ ]*\)dump/\1\/\/ dump/g" $filename $filename
+file=${1:-main.py}
+sed -i -e "s/\(^[ ]*\)db/\1# db/g" $file $file
+sed -i -e "s/\(^[ ]*\)ic/\1# ic/g" $file $file
+sed -i -e "s/^\(from icecream import ic\)/# \1/g" $file $file
+sed -i -e "s/\(^[ ]*\)dump/\1\/\/ dump/g" $file $file
