4ca1c6ff625327b2c381f8a232cf2f61fda052b7
knogu <kotaronoguchi61@gmail.com>
2021-08-21 19:10:55 +0900

    デバッグ出力をコメントインするスクリプトを追加

---
+++ comment_in.sh
@@ -0,0 +1,4 @@
+file=${1:-main.py}
+sed -i -e "s/\(^[ ]*\)\([# ]*\)db/\1db/g" $file $file
+sed -i -e "s/\(^[ ]*\)\([# ]*\)ic/\1ic/g" $file $file
+sed -i -e "s/#[ #]*from icecream import ic/from icecream import ic/g" $file $file
